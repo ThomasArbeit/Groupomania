87450fd54be7297f92be8cddd9c1ef16497663e9
Thomas Arbeit <tarbeit@hotmail.fr>
2020-09-25 21:08:47 +0200

    Ajout authentification sur supp et modif des commentaires

--- middleware/authComm.js
+++ middleware/authComm.js
@@ -1,23 +1,23 @@
 const jwt = require('jsonwebtoken');
-const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     const userRole = decodedToken.role;
-    const userId = decodedToken.userId
-    Post.findAll({
+    const userId = decodedToken.userId;
+    Comment.findAll({
         where: {
-            postId: req.params.id
+            commentId: req.params.id
         }
     })
-    .then(post => {
-        if((post[0].creator_Id === userId) || userRole === 1){
+    .then(comment => {
+        if((comment[0].commentor_Id === userId) || userRole === 1){
             next();
         }
         else{
             res.status(403).json({message: 'Vous ne pouvez pas le supprimer'})
         }
     })
-    .catch(error => res.send("Une erreur est survenue", error))
+    .catch(error => res.status(500).json({error}))
 }
--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/comment');
+const authComm = require('../middleware/authComm')
 const auth = require('../middleware/auth');
 
 // Création d'un post 
@@ -10,9 +11,9 @@ router.post('/create/:id' ,auth,  userCtrl.createOneComment);
 router.get('/:id', auth, userCtrl.getCommentsfromPost);
 
 // Suppression d'un commentaire 
-router.delete('/:id', auth, userCtrl.deleteOneComment);
+router.delete('/:id', auth, authComm, userCtrl.deleteOneComment);
 
 // Modification d'un commentaire
-router.post('/:id', auth, userCtrl.modifyOneComment);
+router.post('/:id', auth, authComm, userCtrl.modifyOneComment);
 
 module.exports = router;
